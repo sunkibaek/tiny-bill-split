57e56615d79042eb0e30d1e1d68a2c48d934695f
Sunki Baek <sunki.baek@gmail.com>
2022-12-05 09:21:56 -0800

    Add slider modal integration

--- src/screens/Home/InputSection.tsx
+++ src/screens/Home/InputSection.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Modal, Text, View } from "react-native";
+import { Modal, Pressable, Text, View } from "react-native";
 import Slider from "@react-native-community/slider";
 
 interface IProps {
@@ -10,17 +10,26 @@ interface IProps {
 }
 
 const InputSection = ({ tip, split, onTipChange }: IProps) => {
-  const [isModalVisible] = useState(true);
+  const [isModalVisible, setIsModalVisible] = useState(false);
+
+  const handleTipPress = () => {
+    setIsModalVisible(true);
+  };
+
+  const handleModalDismiss = () => {
+    setIsModalVisible(false);
+  };
 
   return (
     <View style={{ flexDirection: "row" }}>
-      <View
+      <Pressable
         style={{
           flex: 1,
           alignItems: "center",
           backgroundColor: "#dcbfb9",
           padding: 32,
         }}
+        onPress={handleTipPress}
       >
         <Text>Tip</Text>
 
@@ -33,7 +42,7 @@ const InputSection = ({ tip, split, onTipChange }: IProps) => {
         >
           {tip}%
         </Text>
-      </View>
+      </Pressable>
 
       <View
         style={{
@@ -60,13 +69,31 @@ const InputSection = ({ tip, split, onTipChange }: IProps) => {
         <View
           style={{
             flex: 1,
-            backgroundColor: "rgba(0,0,0,0.5)",
             justifyContent: "center",
             alignItems: "center",
           }}
         >
+          <Pressable
+            onPress={handleModalDismiss}
+            style={{
+              flex: 1,
+              backgroundColor: "rgba(0,0,0,0.5)",
+              position: "absolute",
+              top: 0,
+              bottom: 0,
+              left: 0,
+              right: 0,
+            }}
+          />
+
           <Slider
-            style={{ width: 200, height: 40 }}
+            style={{
+              justifyContent: "center",
+              alignItems: "center",
+              width: 200,
+              height: 40,
+              padding: 32,
+            }}
             minimumValue={0}
             maximumValue={30}
             step={5}
