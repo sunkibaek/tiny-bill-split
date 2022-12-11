c892251af73bbb3fd7bd0271d5bcb6b87a503887
Sunki Baek <sunki.baek@gmail.com>
2022-12-11 13:06:49 -0800

    Refactor to use different action types for special keys

---
+++ src/reducers/keyInputReducer.ts
@@ -0,0 +1,57 @@
+type Mode = "NORMAL" | "DECIMAL";
+
+const MAX_LENGTHS = {
+  cents: 2,
+  dollars: 6,
+};
+
+export const KEY_INPUT_INITIAL_STATE = {
+  dollars: 0,
+  cents: 0,
+  mode: "NORMAL" as Mode,
+};
+
+const keyInputReducer = (
+  state: typeof KEY_INPUT_INITIAL_STATE,
+  action:
+    | { type: "keyInput"; payload: number }
+    | { type: "del" }
+    | { type: "decimal" }
+) => {
+  switch (action.type) {
+    case "decimal":
+      return { ...state, mode: "DECIMAL" };
+    case "del":
+      if (state.cents > 0) {
+        return { ...state, cents: Math.floor(state.cents / 10) };
+      }
+
+      if (state.mode === "DECIMAL") {
+        return { ...state, mode: "NORMAL" };
+      }
+
+      return { ...state, dollars: Math.floor(state.dollars / 10) };
+    case "keyInput":
+      if (String(state.cents).length === MAX_LENGTHS.cents) {
+        return;
+      }
+
+      if (String(state.dollars).length === MAX_LENGTHS.dollars) {
+        return;
+      }
+
+      if (state.mode === "DECIMAL") {
+        return { ...state, cents: state.cents * 10 + action.payload };
+      }
+
+      if (state.mode === "NORMAL") {
+        return { ...state, dollars: state.dollars * 10 + action.payload };
+      }
+
+      return;
+    default:
+      throw new Error(`Unsupported action type: ${action}`);
+  }
+};
+
+export default keyInputReducer;
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,86 +1,33 @@
 import { useReducer, useState } from "react";
 import { View } from "react-native";
 
+import keyInputReducer, {
+  KEY_INPUT_INITIAL_STATE,
+} from "../../reducers/keyInputReducer";
 import InputSection from "./InputSection";
 import NumpadSection from "./NumpadSection";
 import ResultSection from "./ResultSection";
 
-type Mode = "NORMAL" | "DECIMAL";
-
-const INITIAL_STATE: {
-  dollars: number;
-  cents: number;
-  mode: Mode;
-} = {
-  dollars: 0,
-  cents: 0,
-  mode: "NORMAL",
-};
-
-const reducer = (
-  state: typeof INITIAL_STATE,
-  {
-    type,
-    payload,
-  }: {
-    type: "keyInput";
-    payload: NumType;
-  }
-) => {
-  switch (type) {
-    case "keyInput":
-      if (payload === ".") {
-        return { ...state, mode: "DECIMAL" };
-      }
-
-      if (payload === "DEL") {
-        if (state.cents > 0) {
-          return { ...state, cents: Math.floor(state.cents / 10) };
-        }
-
-        if (state.mode === "DECIMAL") {
-          return { ...state, mode: "NORMAL" };
-        }
-
-        return { ...state, dollars: Math.floor(state.dollars / 10) };
-      }
-
-      if (typeof payload !== "number") {
-        throw new Error("Unsupported input entered.");
-      }
-
-      if (String(state.cents).length === 2) {
-        return;
-      }
-
-      if (String(state.dollars).length === 6) {
-        return;
-      }
-
-      if (state.mode === "DECIMAL") {
-        return { ...state, cents: state.cents * 10 + payload };
-      }
-
-      if (state.mode === "NORMAL") {
-        return { ...state, dollars: state.dollars * 10 + payload };
-      }
-
-      return;
-    default:
-      throw new Error(`Unsupported action type: ${type}`);
-  }
-};
-
 const Home = () => {
   const [{ dollars, cents, mode }, dispatch] = useReducer(
-    reducer,
-    INITIAL_STATE
+    keyInputReducer,
+    KEY_INPUT_INITIAL_STATE
   );
 
   const [tip, setTip] = useState(15);
   const [split, setSplit] = useState(1);
 
   const handleNumPress = (num: NumType) => {
+    if (num === ".") {
+      dispatch({ type: "decimal" });
+      return;
+    }
+
+    if (num === "DEL") {
+      dispatch({ type: "del" });
+      return;
+    }
+
     dispatch({ type: "keyInput", payload: num });
   };
 
