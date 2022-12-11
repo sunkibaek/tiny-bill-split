14fa77069822604aa363f7d4a4574ad1b92cb3e5
Sunki Baek <sunki.baek@gmail.com>
2022-12-11 12:26:26 -0800

    Extract formatAmount function

--- src/components/Money.tsx
+++ src/components/Money.tsx
@@ -7,11 +7,17 @@ interface IProps {
 
 const CENT_DIGIT = 2;
 
-const Money = ({ amountInCents, style }: IProps) => {
+const formatAmount = (amountInCents: number) => {
   const dollarAmount = Math.round(amountInCents) / 10 ** CENT_DIGIT;
   const amountString = String(dollarAmount);
   let [dollars, cents] = amountString.split(".");
   cents = (cents || "").padEnd(CENT_DIGIT, "0");
+
+  return { dollars, cents };
+};
+
+const Money = ({ amountInCents, style }: IProps) => {
+  const { dollars, cents } = formatAmount(amountInCents);
 
   return (
     <Text style={style}>
